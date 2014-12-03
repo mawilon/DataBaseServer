96d0fa15c1d4799690afc3d299438ac561b1bde3
mawilon <mawilon@outlook.com>
2014-12-03 17:34:19 +0800

    修改DBEntity

--- DBServer/OperateDB/DBEntity/DBConnProperty.cs
+++ DBServer/OperateDB/DBEntity/DBConnProperty.cs
@@ -8,5 +8,29 @@ namespace OperateDB.DBEntity
 {
     public class DBConnProperty
     {
+        /// <summary>
+        /// 数据库类型
+        /// </summary>
+        public int DB_Type { get; set; }
+
+        /// <summary>
+        /// 数据库名称
+        /// </summary>
+        public string DB_Name { get; set; }
+
+        /// <summary>
+        /// 数据库地址
+        /// </summary>
+        public string DB_DataSource { get; set; }
+
+        /// <summary>
+        /// 请求用户
+        /// </summary>
+        public string DB_UserID { get; set; }
+
+        /// <summary>
+        /// 用户密码
+        /// </summary>
+        public string DB_Password { get; set; }
     }
 }
