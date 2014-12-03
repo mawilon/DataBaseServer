696538fa597e7c1fa0b8906f30fb8f66c6ae47af
mawilon <mawilon@outlook.com>
2014-12-03 17:37:10 +0800

    再提交一次

---
+++ DBServer/OperateDB/DBEntity/SqlDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperateDB.DBEntity
+{
+    public class SqlDB
+    {
+        /// <summary>
+        /// 数据库类型
+        /// </summary>
+        public int DB_Type { get; set; }
+
+        /// <summary>
+        /// 数据库
+        /// </summary>
+        public string SB_Name { get; set; }
+    }
+}
